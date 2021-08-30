fd513e62cb54f1f1c15fd36343adb74b3efd9862
tlatmdxo123 <tmdxosmswkd@gmail.com>
2021-08-30 12:04:59 +0900

    set webpack mode in dev & production

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,38 +1,43 @@
 const path = require('path')
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-  mode: "development",
-  entry: "./index.tsx",
-  output: {
-    path: path.resolve(__dirname, "public"),
-    filename: "main.js",
-  },
-  target: "node",
-  devServer: {
-    port: "9500",
-    contentBase: ["./public"],
-    historyApiFallback: true,
-    open: true,
-  },
-  resolve: {
-    extensions: [".js", ".jsx", ".json", ".ts", ".tsx"],
-  },
+module.exports = (env) => {
+  console.log(env)
+  const isProduction = env ==='production';
+  return {
+    mode: "development",
+    entry: "./index.tsx",
+    output: {
+      path: path.resolve(__dirname, "public"),
+      filename: "main.js",
+    },
+    target: "node",
+    devServer: {
+      port: "9500",
+      contentBase: ["./public"],
+      historyApiFallback: true,
+      open: true,
+    },
+    resolve: {
+      extensions: [".js", ".jsx", ".json", ".ts", ".tsx"],
+    },
 
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx|ts|tsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx|ts|tsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+          },
         },
-      },
-      {
-        test: /\.scss$/,
-        exclude: /node_modules/,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
-    ],
-  },
-};
+        {
+          test: /\.scss$/,
+          exclude: /node_modules/,
+          use: ["style-loader", "css-loader", "sass-loader"],
+        },
+      ],
+    },
+    devtool:isProduction ? 'source-map' : 'inline-source-map'
+  };
+}
